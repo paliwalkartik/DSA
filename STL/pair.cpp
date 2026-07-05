499f4b16b51114632227ae036ac43c0c18681fd5
paliwalkartik <paliwalkartik72@gmail.com>
2026-07-05 15:27:50 +0000

    Patterns and STL done 5/07/26

---
+++ STL/pair.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  //  pair<int,int>p = {1,5};
+   // cout<<p.first<<" "<<p.second;
+ //  pair<int,pair<int,int>>p = {1,{5,8}};
+   //cout<<p.first<<" "<<p.second.first<<" "<<p.second.second;
+     pair<int,int>arr[] = {{1,4},{3,6},{3,8}};
+     cout<<arr[2].second;
+}
---
+++ STL/vector.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   vector<int>v;
+    v.push_back(4);
+    v.emplace_back(7);
+   cout<<v[0];
+  // vector<pair<int,int>>v;
+  // v.push_back({1,5});
+  // v.emplace_back(4,8);
+  // cout<<v[1].second;
+  //vector<int>::iterator it = v.begin();
+  //it++;
+  //cout<<*(it)<<" ";
+  //vector<int>::iterator it = v.end();
+  //cout<<v.back()<<" ";
+
+  //for(vector<int>::iterator it = v.begin();it!=v.end();it++)
+  //cout<<*(it)<<" ";
+  //for(auto it = v.begin();it!=v.end();it++){
+    //cout<<*(it)<<" ";
+
+  //   v.erase(v.begin()+1);
+
+  //  for(auto &it : v){
+  //      cout<<it<<" ";
+  //  }
+  
+  }
