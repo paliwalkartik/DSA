ad83b2f5f9c650f3ece45e7cc5365e261fdb7965
paliwalkartik <paliwalkartik72@gmail.com>
2026-07-06 16:39:15 +0000

    Sorting done

--- Algorithm/Sort.cpp
+++ Algorithm/Sort.cpp
@@ -7,24 +7,37 @@ using namespace std;
   //  for(auto &it : v){
     //    cout<<it.first<< " "<<it.second<<" ";
    // }
-    bool comp(pair<int,int>p1,pair<int,int>p2){
-        if(p1.second<p2.second)return true;
-        if(p1.second>p2.second)return false;
+  //  bool comp(pair<int,int>p1,pair<int,int>p2){
+   //     if(p1.second<p2.second)return true;
+  //      if(p1.second>p2.second)return false;
+//
+  //      if(p1.first>p2.first)return true;
+ //       return false;
+ //   }
+ //   int main(){
+ //       vector<pair<int,int>>v={{1,3},{3,3},{4,1}};
+ //       sort(v.begin(),v.end(),comp);
+  //      for(auto &it : v){
+  //          cout<<it.first<<" "<<it.second<<" ";
+  //      }
 
-        if(p1.first>p2.first)return true;
-        return false;
-    }
-    int main(){
-        vector<pair<int,int>>v={{1,3},{3,3},{4,1}};
-        sort(v.begin(),v.end(),comp);
-        for(auto &it : v){
-            cout<<it.first<<" "<<it.second<<" ";
-        }
-
-    }
-
-  
-
-
-
+   // }
 // }
+
+
+
+
+int main(){
+ //   int num = 7;
+ //   cout<< __builtin_popcount(num)<<endl;
+
+// string s = "312";
+ //sort(s.begin(),s.end());
+ //do{
+   // cout<<s<<endl;
+ //}while(next_permutation(s.begin(),s.end()));
+ vector<int>v= {2,5,10,23,45};
+
+ int max=*max_element(v.begin(),v.end());
+ cout<<max<<endl;
+}
